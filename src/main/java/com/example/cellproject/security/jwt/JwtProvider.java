8ae8718e95b43dbe17fd7ce60b40b05cedea5f63
김윤환 <49113950+kyh5037@users.noreply.github.com>
2023-03-25 13:27:27 +0900

    [Spring Security] jwt token AuthorizationFilter create

--- src/main/java/com/example/cellproject/security/jwt/JwtProvider.java
+++ src/main/java/com/example/cellproject/security/jwt/JwtProvider.java
@@ -10,7 +10,7 @@ import io.jsonwebtoken.security.Keys;
 import jakarta.annotation.PostConstruct;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import lombok.Value;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -26,6 +26,9 @@ import java.util.List;
 @Component
 public class JwtProvider {
 
+    // 토큰의 암호화 복호화를 위한 key
+    // application.properties에서 secret key를 가져올 수 있다.
+    @Value("${jwt.secret.key}")
     private String salt;
 
     private Key secretKey;
@@ -36,19 +39,20 @@ public class JwtProvider {
 
     @PostConstruct // 종속성 주입이 완료된 후 실행되어야 하는 메서드
     protected void init(){
+        // salt를 byte배열로 전달하며, SecretKey 인스턴스를 생성
         secretKey = Keys.hmacShaKeyFor(salt.getBytes(StandardCharsets.UTF_8));
     }
 
-    // 토큰 생성
+    // 토큰 생성(Token은 String으로 구성되기 때문에 반환타입 String)
     public String createToken(String usrId, List<Authority> roles) {
-        Claims claims = Jwts.claims().setSubject(usrId);
-        claims.put("roles", roles);
+        Claims claims = Jwts.claims().setSubject(usrId); // Claim : 사용자에 대한 프로퍼티나 속성, 제목
+        claims.put("roles", roles); // claim 에 담을 값(key, value)
         Date now = new Date();
         return Jwts.builder()
                 .setClaims(claims)
-                .setIssuedAt(now)
-                .setExpiration(new Date(now.getTime() + exp))
-                .signWith(secretKey, SignatureAlgorithm.HS256)
+                .setIssuedAt(now) // 발행일
+                .setExpiration(new Date(now.getTime() + exp)) // 만료일
+                .signWith(secretKey, SignatureAlgorithm.RS256) // signature
                 .compact();
     }
 
@@ -60,7 +64,9 @@ public class JwtProvider {
 
     // 토큰에 담겨있는 유저Id 획득
     public String getUsrId(String token) {
-        return Jwts.parserBuilder().setSigningKey(secretKey).build().parseClaimsJws(token).getBody().getSubject();
+        return Jwts.parserBuilder()
+                .setSigningKey(secretKey)
+                .build().parseClaimsJws(token).getBody().getSubject();
     }
 
     // Authorization Header를 통해 인증을 한다.
@@ -77,7 +83,9 @@ public class JwtProvider {
             } else {
                 token = token.split(" ")[1].trim();
             }
-            Jws<Claims> claims = Jwts.parserBuilder().setSigningKey(secretKey).build().parseClaimsJws(token);
+            Jws<Claims> claims = Jwts.parserBuilder()
+                                    .setSigningKey(secretKey)
+                                    .build().parseClaimsJws(token);
             // 만료되었을 시 false
             return !claims.getBody().getExpiration().before(new Date());
         } catch (Exception e) {
